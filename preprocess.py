c112c3af056d6bab688f169342c1ff03c9853526
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2018-12-27 17:46:17 +0200

    Added statistics for each move pair. Added preprocess for test logs. Updated readme file.

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,22 @@
+import itertools
 import json
 import os
 import shutil
 from collections import OrderedDict
 
 
-def find_util(profile, bid):
+def find_util(profile, bid, issues):
     """
     Finds the utility for a bid, given the preference profile
     :param profile: The preference profile of the agent
     :param bid: The bid that is examined
+    :param issues: The issues of the negotiation
     :return: The utility of the agent for the given bid
     """
     bid = bid.split(",")
     utility = 0
     for index, issue in enumerate(issues):
-        utility += profile[issue]["weight"]*profile[issue][bid[index]]
+        utility += profile[issue]["weight"] * profile[issue][bid[index]]
     return utility
 
 
@@ -44,80 +46,119 @@ def find_move_type(u1_curr, u1_prev, u2_curr, u2_prev):
         move = "concession"
     return move
 
-# create the directory for the updated training logs
-result_dir = "training_logs_updated"
-if os.path.isdir(result_dir):
-    shutil.rmtree(result_dir)
-os.makedirs(result_dir)
+# mapping of moves and pair of moves to numbers
+move_mapping = {"concession": 1, "fortunate": 2, "nice": 3, "selfish": 4, "silent": 5, "unfortunate": 6}
+
+moves_product = list(
+    itertools.product(["nice", "silent", "fortunate", "unfortunate", "selfish", "concession"], repeat=2))
+move_pair_mapping = dict([(y, x + 1) for x, y in enumerate(sorted(set(moves_product)))])
 
 
-for root, dirs, files in os.walk("training_logs", topdown=False):   # run for all the training logs
-    for log in files:
-        with open(os.path.join(root,log), "r") as fin:
-            data = json.load(fin, object_pairs_hook=OrderedDict)
-        issues = data["issues"]
-        bids = data["bids"]
-        profile_1 = data["Utility1"]
-        profile_2 = data["Utility2"]
+def update_logs(d):
+    """
+    Update the logs with each move's type and some statistics about them.
+    :param d: The directory of the log files
+    """
+    # create the directory for the updated logs
+    new_dir = d + "_updated"
+    if os.path.isdir(new_dir):
+        shutil.rmtree(new_dir)
+    os.makedirs(new_dir)
 
-        kind_of_moves1 = {}
-        kind_of_moves2 = {}
-        num_of_moves1 = 0
-        num_of_moves2 = 0
+    for root, dirs, files in os.walk(d, topdown=False):  # run for all the training logs
+        for log in files:
+            with open(os.path.join(root, log), "r") as fin:
+                data = json.load(fin, object_pairs_hook=OrderedDict)
 
-        for ind, bid in enumerate(bids[1:]):    # find each move's type, except form the first one
-            moves = []
-            for agent in ["agent1", "agent2"]:  # run for each agent in  every round
-                # in the round that an agent accepts an offer, the training log
-                # has the following format:
-                # "round": "143",
-                # "agent1": "Apples,Milk,Almonds,None",
-                # "accept": "agent2"
-                # so we can't find the move's type
-                if agent in bid:
-                    if agent == "agent1":
-                        prof = profile_1
-                        opp = profile_2
+            issues = data["issues"]
+            bids = data["bids"]
+            profile_1 = data["Utility1"]
+            profile_2 = data["Utility2"]
+
+            kind_of_moves1 = {}
+            kind_of_moves2 = {}
+            kind_of_moves_both = {}
+            num_of_moves1 = 0
+            num_of_moves2 = 0
+            num_of_moves_both = 0
+
+            for ind, bid in enumerate(bids[1:]):  # find each move's type, except form the first one
+                moves = []
+                for agent in ["agent1", "agent2"]:  # run for each agent in  every round
+                    # in the round that an agent accepts an offer, the training log
+                    # has the following format:
+                    # "round": "143",
+                    # "agent1": "Apples,Milk,Almonds,None",
+                    # "accept": "agent2"
+                    # so we can't find the move's type
+                    if agent in bid:
+                        if agent == "agent1":
+                            prof = profile_1
+                            opp = profile_2
+                        else:
+                            prof = profile_2
+                            opp = profile_1
+                        u1_curr = find_util(prof, bid[agent], issues)
+                        u1_prev = find_util(prof, bids[ind][agent], issues)
+
+                        u2_curr = find_util(opp, bid[agent], issues)
+                        u2_prev = find_util(opp, bids[ind][agent], issues)
+
+                        move = find_move_type(u1_curr, u1_prev, u2_curr, u2_prev)
+                        moves.append(move)
                     else:
-                        prof = profile_2
-                        opp = profile_1
-                    u1_curr = find_util(prof, bid[agent])
-                    u1_prev = find_util(prof, bids[ind][agent])
+                        moves.append(None)
 
-                    u2_curr = find_util(opp, bid[agent])
-                    u2_prev = find_util(opp, bids[ind][agent])
+                # update the dictionary with the type of the move
+                # compute the frequency of each move type
+                if moves[0]:
+                    num_of_moves1 += 1
+                    move_num = move_mapping[moves[0]]
+                    if move_num in kind_of_moves1:
+                        kind_of_moves1[move_num] += 1
+                    else:
+                        kind_of_moves1[move_num] = 1
+                    data["bids"][ind + 1]["move1"] = [moves[0], move_num]
+                if moves[1]:
+                    num_of_moves2 += 1
+                    move_num = move_mapping[moves[1]]
+                    if move_num in kind_of_moves2:
+                        kind_of_moves2[move_num] += 1
+                    else:
+                        kind_of_moves2[move_num] = 1
+                    data["bids"][ind + 1]["move2"] = [moves[1], move_num]
 
-                    move = find_move_type(u1_curr, u1_prev, u2_curr, u2_prev)
-                    moves.append(move)
-                else:
-                    moves.append(None)
+                if moves[0] and moves[1]:
+                    num_of_moves_both += 1
+                    move_pair_num = move_pair_mapping[(moves[0], moves[1])]
+                    data["bids"][ind + 1]["move_pair_num"] = [(moves[0], moves[1]), move_pair_num]
+                    if move_pair_num in kind_of_moves_both:
+                        kind_of_moves_both[move_pair_num] += 1
+                    else:
+                        kind_of_moves_both[move_pair_num] = 1
 
-            # update the dictionary with the type of the move
-            # compute the frequency of each move type
-            if moves[0]:
-                num_of_moves1 += 1
-                if moves[0] in kind_of_moves1:
-                    kind_of_moves1[moves[0]] += 1
-                else:
-                    kind_of_moves1[moves[0]] = 1
-                data["bids"][ind+1]["move1"] = moves[0]
-            if moves[1]:
-                num_of_moves2 += 1
-                if moves[1] in kind_of_moves2:
-                    kind_of_moves2[moves[1]] += 1
-                else:
-                    kind_of_moves2[moves[1]] = 1
-                data["bids"][ind+1]["move2"] = moves[1]
+            for move, times in kind_of_moves1.items():
+                kind_of_moves1[move] = "{0:.2f}".format(float(kind_of_moves1[move]) / num_of_moves1)
 
-        for move, times in kind_of_moves1.items():
-            kind_of_moves1[move] = "{0:.2f}".format(float(kind_of_moves1[move]) / num_of_moves1)
+            for move, times in kind_of_moves2.items():
+                kind_of_moves2[move] = "{0:.2f}".format(float(kind_of_moves2[move]) / num_of_moves2)
 
-        for move, times in kind_of_moves2.items():
-            kind_of_moves2[move] = "{0:.2f}".format(float(kind_of_moves2[move]) / num_of_moves2)
+            for move, times in kind_of_moves_both.items():
+                kind_of_moves_both[move] = "{0:.2f}".format(float(kind_of_moves_both[move]) / num_of_moves_both)
 
-        data["kind_of_moves1"] = kind_of_moves1
-        data["kind_of_moves2"] = kind_of_moves2
+            data["kind_of_moves1"] = kind_of_moves1
+            data["kind_of_moves2"] = kind_of_moves2
+            data["kind_of_moves_both"] = kind_of_moves_both
 
-        # save the updated training log
-        with open(os.path.join(result_dir, "updated_" + log), "w") as fout:
-            json.dump(data, fout, indent=4, separators=(',', ': '))
+            # save the updated training log
+            with open(os.path.join(new_dir, "updated_" + log), "w") as fout:
+                json.dump(data, fout, indent=4, separators=(',', ': '))
+
+
+# the directories of tha train and test logs
+test_dir = "test_logs"
+train_dir = "training_logs"
+
+# call the functions that update the logs
+update_logs(train_dir)
+update_logs(test_dir)
