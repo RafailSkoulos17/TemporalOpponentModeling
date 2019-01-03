7d88955e8a05d00ab06e49554465959d10f137df
LENOVO-PC\Vasilis <el12021@central.ntua.gr>
2019-01-03 23:46:50 +0200

    Final version (with all the commenting) added for the training script

--- train_final.py
+++ train_final.py
@@ -49,7 +49,7 @@ def find_move_type(u1_curr, u1_prev, u2_curr, u2_prev):
     return move
 
 
-# mapping of moves and pair of moves to numbers
+# The mapping of moves and pair of moves to numbers
 move_mapping = {"concession": 1, "fortunate": 2, "nice": 3, "selfish": 4, "silent": 5, "unfortunate": 6}
 
 moves_product = list(
@@ -58,16 +58,22 @@ move_pair_mapping = dict([(y, x + 1) for x, y in enumerate(sorted(set(moves_prod
 
 
 def find_moves_and_utils(ind, issues, profile_1, profile_2, bids, bid):
+    """
+        Finds the moves made in each bid
+        :param ind: The index of the examined bid in the list of bids of the negotiation
+        :param issues: The issues in the current negotiation
+        :param profile_1: The profile of agent 1
+        :param profile_2: The profile of agent 2
+        :param bids: The bids of the negotiation
+        :param bid: The currently examined bid
+        :return: The moves of the two agents, and the difference of the utilities of the bids made in the current round
+                 If its the second round also diff_prev is returned with the utilities of the first round. These
+                 utilities were examined as features at the beginning but the results were not so promising.
+        """
     moves = []
     diff = []
     diff_prev = []
-    for agent in ["agent1", "agent2"]:  # run for each agent in  every round
-        # in the round that an agent accepts an offer, the training log
-        # has the following format:
-        # "round": "143",
-        # "agent1": "Apples,Milk,Almonds,None",
-        # "accept": "agent2"
-        # so we can't find the move's type
+    for agent in ["agent1", "agent2"]:
         if agent in bid:
             if agent == "agent1":
                 prof = profile_1
@@ -94,8 +100,13 @@ def find_moves_and_utils(ind, issues, profile_1, profile_2, bids, bid):
 
 def update_logs(d, type):
     """
-    Update the logs with each move's type and some statistics about them.
+    Produces the needed features. In the current implementation it creates the list of combined moves for each type
+    of agent (conceder, hardheaded, random, tft)
     :param d: The directory of the log files
+    :param type: The type of agents for which the training set will be created
+    :return: The training set for the wanted type of both the combined moves and just the simple moves. Also it returns
+             the utilities of the bids for the needed class. In the current implementation only the combined moves
+             are used as observations
     """
 
     for root, dirs, files in os.walk(d, topdown=False):  # run for all the training logs
@@ -103,7 +114,7 @@ def update_logs(d, type):
         train_naive = []
         train_naive_utils = []
         for log in files:
-            if str(log).count(type) == 2:
+            if str(log).count(type) == 2: # if both agents are of the wanted type
                 with open(os.path.join(root, log), "r") as fin:
                     data = json.load(fin, object_pairs_hook=OrderedDict)
 
@@ -194,7 +205,7 @@ def update_logs(d, type):
     return train_combined, train_naive, train_naive_utils
 
 
-# function used to normalize the data so that they fit the gaussian standards (-1,1)
+# function used to normalize the data so that they fit the Gaussian standards (-1,1)
 def normalize(data):
     normalized = []
     for d in data:
@@ -205,7 +216,7 @@ def normalize(data):
     return normalized
 
 
-# another function to change the formatting
+# Function to change the formatting of the data
 def transform(lst):
     out = []
     for l in lst:
@@ -214,7 +225,7 @@ def transform(lst):
     return out
 
 
-# the main function that produces the observations in the needed format accompanied by the length of each sequence
+# The main function that produces the observations in the needed format accompanied by the length of each sequence
 def create_obs(data):
     normalized = normalize(data)
     lengths = list(map(lambda x: len(x), data))
@@ -222,17 +233,16 @@ def create_obs(data):
     return obs,lengths
 
 
-# function used to train the hmm - only the prior probabilities are initialized
+# Function used to train the hmm - only the prior probabilities are initialized
 def create_and_fit_hmm(obs, lengths):
-    model = hmm.GaussianHMM(n_components=2, n_iter=50)
+    model = hmm.GaussianHMM(n_components=2)
     model.startprob_ = np.array([0.5, 0.5])
     model.fit(obs,lengths)
     return model
 
 
-# function used to apply a type of leave-one-out cross validation to each category so that a better estimation
-# of the counts of each class to be derived (not an actual cross validation since the whole training set
-# isn't used
+# Function used to apply a type of leave-one-out cross validation to each category so that a better estimation
+# of the counts of each class to be derived
 def my_cross_val(obs, lengths):
     start = 0
     count0 = 0
@@ -244,7 +254,6 @@ def my_cross_val(obs, lengths):
         res = model.predict(obs[start:start+ln])
         count0 += list(res).count(0)
         count1 += list(res).count(1)
-        #print(start)
         start += ln
     return count0, count1
 
@@ -253,66 +262,134 @@ def my_cross_val(obs, lengths):
 The main part of the calculations start here.
 For each model the observations and the lengths are calculated 
 Then each model is trained and the fitted counts are produced
-After that also the "cross validated" counts are produced
-Finally the testing is performed by comparing the relative differences 
-produced by the test cases to those produced by both the fitted sets 
-and the cross validated ones.
+After that also the "validated" counts are produced. We guarantee the validity of these counts by checking 
+their relative differences from those of the fitted counts. 
 '''
 
 # conceder model
+# train_naive and train_naive_utils are not eventually used since they were just some other possible features for use
 train_combined, train_naive, train_naive_utils = update_logs("training_logs", "conceder")
 obs1, lengths1 = create_obs(train_combined)
 conceder_model = create_and_fit_hmm(obs1, lengths1)
 pred = list(conceder_model.predict(obs1))
-print("Conceder fitted:",pred.count(0),pred.count(1))
-conceder_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1))
+#print("Conceder fitted:",pred.count(0),pred.count(1))
+conceder_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1)) # calculation of the fitted relative difference
 
 # hardheaded
 train_combined, train_naive, train_naive_utils = update_logs("training_logs", "hardheaded")
 obs2, lengths2 = create_obs(train_combined)
 hardheaded_model = create_and_fit_hmm(obs2, lengths2)
 pred = list(hardheaded_model.predict(obs2))
-print("HardHeaded fitted:",pred.count(0),pred.count(1))
-hardheaded_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1))
+#print("HardHeaded fitted:",pred.count(0),pred.count(1))
+hardheaded_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1)) # calculation of the fitted relative difference
 
 # random model
 train_combined, train_naive, train_naive_utils = update_logs("training_logs", "random")
 obs3, lengths3 = create_obs(train_combined)
 random_model = create_and_fit_hmm(obs3, lengths3)
 pred = list(random_model.predict(obs3))
-print("Random fitted:",pred.count(0),pred.count(1))
-random_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1))
+#print("Random fitted:",pred.count(0),pred.count(1))
+random_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1)) # calculation of the fitted relative difference
 
 # tft model
 train_combined, train_naive, train_naive_utils = update_logs("training_logs", "tft")
 obs4, lengths4 = create_obs(train_combined)
 tft_model = create_and_fit_hmm(obs4, lengths4)
 pred = list(tft_model.predict(obs4))
-print("TFT fitted:",pred.count(0),pred.count(1))
-tft_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1))
+#print("TFT fitted:",pred.count(0),pred.count(1))
+tft_diff = (pred.count(0)-pred.count(1))/(pred.count(0)+pred.count(1)) # calculation of the fitted relative difference
 
-print("---------------------------------------------")
+#print("---------------------------------------------")
 
-# validating
-count0, count1 = my_cross_val(obs1, lengths1)
-conceder_diff_val = (count0 - count1)/(count0+count1)
-print("Conceder Validation:", count0, count1)
-count0, count1 = my_cross_val(obs2, lengths2)
-hardheaded_diff_val = (count0 - count1)/(count0+count1)
-print("HardHeaded Validation:", count0, count1)
-count0, count1 = my_cross_val(obs3, lengths3)
-random_diff_val = (count0 - count1)/(count0+count1)
-print("Random Validation:", count0, count1)
-count0, count1 = my_cross_val(obs4, lengths4)
-tft_diff_val = (count0 - count1)/(count0+count1)
-print("TFT Validation:", count0, count1)
+# validating section
+flag = False # flag to show that the validation "converged"
+counts = 0 # a value used to ensure to loosen the criterion of stopping the validation procedure
+threshold = 0.05 # the initial threshold of the difference between the validated and the fitted data
+best = 100 # a value used just to ensure the termination of the procedure with the most decently converged value
+while not flag:
+    if counts > 5: # increase the threshold after 6 attempts are made
+        counts = 0
+        threshold += 0.01
+    count0, count1 = my_cross_val(obs1, lengths1)
+    conceder_diff_val = (count0 - count1)/(count0+count1) # calculation of the validated relative difference
+    if abs(conceder_diff_val - conceder_diff) < best: # keep the best one so far just in case the procedure takes too long
+        best = abs(conceder_diff_val - conceder_diff)
+        best_val = conceder_diff_val
+    if abs(conceder_diff_val-conceder_diff) < threshold:
+        flag = True
+    counts += 1
+    if threshold > 0.1 and not flag: # if the procedure takes too long give the best value so far
+        flag = True
+        conceder_diff_val = best_val
+#print("Conceder Validation:", count0, count1)
+flag = False
+counts = 0
+threshold = 0.05
+best = 100
+while not flag:
+    if counts > 5: # increase the threshold after 6 attempts are made
+        counts = 0
+        threshold += 0.01
+    count0, count1 = my_cross_val(obs2, lengths2)
+    hardheaded_diff_val = (count0 - count1)/(count0+count1) # calculation of the validated relative difference
+    if abs(hardheaded_diff_val - hardheaded_diff) < best: # keep the best one so far just in case the procedure takes too long
+        best = abs(hardheaded_diff_val - hardheaded_diff)
+        best_val = hardheaded_diff_val
+    if abs(hardheaded_diff_val - hardheaded_diff) < threshold:
+        flag = True
+    counts += 1
+    if threshold > 0.1 and not flag: # if the procedure takes too long give the best value so far
+        flag = True
+        hardheaded_diff_val = best_val
+#print("HardHeaded Validation:", count0, count1)
+flag = False
+counts = 0
+threshold = 0.05
+best = 100
+while not flag:
+    if counts > 5: # increase the threshold after 6 attempts are made
+        counts = 0
+        threshold += 0.01
+    count0, count1 = my_cross_val(obs3, lengths3)
+    random_diff_val = (count0 - count1)/(count0+count1) # calculation of the validated relative difference
+    if abs(random_diff_val - random_diff) < best: # keep the best one so far just in case the procedure takes too long
+        best = abs(random_diff_val - random_diff)
+        best_val = random_diff_val
+    if abs(random_diff_val - random_diff) < threshold:
+        flag = True
+    counts += 1
+    if threshold > 0.1 and not flag: # if the procedure takes too long give the best value so far
+        flag = True
+        random_diff_val = best_val
+#print("Random Validation:", count0, count1)
+flag = False
+counts = 0
+threshold = 0.05
+best = 100
+while not flag:
+    if counts > 5: # increase the threshold after 6 attempts are made
+        counts = 0
+        threshold += 0.01
+    count0, count1 = my_cross_val(obs4, lengths4)
+    tft_diff_val = (count0 - count1)/(count0+count1) # calculation of the validated relative difference
+    if abs(tft_diff_val - tft_diff) < best: # keep the best one so far just in case the procedure takes too long
+        best = abs(tft_diff_val - tft_diff)
+        best_val = tft_diff_val
+    if abs(tft_diff_val - tft_diff) < threshold:
+        flag = True
+    counts += 1
+    if threshold > 0.1 and not flag: # if the procedure takes too long give the best value so far
+        flag = True
+        tft_diff_val = best_val
 
+#print("TFT Validation:", count0, count1)
+
+# finally the model and the produced differences are saved to be used for testing
 to_save = [conceder_model, hardheaded_model,
            random_model, tft_model,
            conceder_diff, hardheaded_diff,
            random_diff, tft_diff,
            conceder_diff_val, hardheaded_diff_val,
            random_diff_val, tft_diff_val]
-#print(to_save)
 with open("models_metrics.txt", "wb") as fp:
     pickle.dump(to_save, fp)
