5c95d6869255c9b4653ad4d285f8c98bcd1c2726
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2019-01-04 16:14:00 +0200

    Added comments in testing script.

--- test_final.py
+++ test_final.py
@@ -1,11 +1,9 @@
 import itertools
 import json
-import os
-from collections import OrderedDict
-
-import numpy as np
 import pickle
 import sys
+import numpy as np
+from collections import OrderedDict
 
 
 def find_util(profile, bid, issues):
@@ -59,6 +57,22 @@ move_pair_mapping = dict([(y, x + 1) for x, y in enumerate(sorted(set(moves_prod
 
 
 def find_moves_and_utils(ind, issues, profile_1, profile_2, bids, bid):
+    """
+    Finds the type of the moves for the 2 agents and the difference between
+    the utility achieved for the 2 agents in the current round.If the round
+    examined is the 2nd one(ind==0), then the difference
+     between the utilities for the 1st round is also computed.
+    :param ind: The index of the bids of the previous round.
+    :param issues: The issues of the negotiation
+    :param profile_1: The preference profile of the 1st agent
+    :param profile_2: The preference profile of the 2nd agent
+    :param bids: All the bids of the negotiation
+    :param bid: The bid of the current round
+    :return: The type of the moves for the 2 agents, the difference
+     between the utilities for the 2 agents in the the current round.
+     If the round examined is the 2nd one(ind==0), then the difference
+     between the utilities for the 1st round is also returned.
+    """
     moves = []
     diff = []
     diff_prev = []
@@ -76,11 +90,11 @@ def find_moves_and_utils(ind, issues, profile_1, profile_2, bids, bid):
             else:
                 prof = profile_2
                 opp = profile_1
-            u1_curr = find_util(prof, bid[agent], issues)
-            u1_prev = find_util(prof, bids[ind][agent], issues)
+            u1_curr = find_util(prof, bid[agent], issues)   # current utility of the examined agent
+            u1_prev = find_util(prof, bids[ind][agent], issues)     # previous utility of the examined agent
 
-            u2_curr = find_util(opp, bid[agent], issues)
-            u2_prev = find_util(opp, bids[ind][agent], issues)
+            u2_curr = find_util(opp, bid[agent], issues)    # current utility of the opponent agent
+            u2_prev = find_util(opp, bids[ind][agent], issues)  # previous utility of the opponent agent
 
             diff.append(u1_curr - u2_curr)
             if ind == 0:
@@ -95,12 +109,17 @@ def find_moves_and_utils(ind, issues, profile_1, profile_2, bids, bid):
 
 def update_logs(filename):
     """
-    Update the logs with each move's type and some statistics about them.
-    :param filename: The file that contains the negotiation  logs we want to update
+    Examine a negotiation log and returns the move pairs for each round, the type of
+    move for each agent in every round and the difference between the utilities for
+    the 2 agents in every round.
+    :param filename: The file that contains the negotiation logs we want to examine.
+    :return: A list with the move pair for each round encoded, a list with the move
+    of each agent in each round encoded, and a list with the difference between the
+    utilities for the 2 agents in every round.
     """
-    test_combined = []  # the lists to store both the combination of moves and each move separately
-    test_naive = []
-    test_naive_utils = []
+    test_combined = []  # the list to store the combination of moves
+    test_naive = []     # the list to store each move separately
+    test_naive_utils = []   # the list to store the difference between the utility achieved for the 2 agents
 
     with open(filename, "r") as fin:
         data = json.load(fin, object_pairs_hook=OrderedDict)
@@ -111,17 +130,17 @@ def update_logs(filename):
     profile_2 = data["Utility2"]
 
     temp_agent1 = []
-    temp_agent2 = []  # used in case that we have agents from the same class negotiating
+    temp_agent2 = []
     temp1 = []
-    temp2 = []  # used in case that we have agents from the same class negotiating
+    temp2 = []
     temp_util1 = []
     temp_util2 = []
 
     for ind, bid in enumerate(bids[1:]):  # find each move's type, except form the first one
 
         moves, diff, diff_prev = find_moves_and_utils(ind, issues, profile_1, profile_2, bids, bid)
-        # update the dictionary with the type of the move
-        # compute the frequency of each move type
+
+        # update the list with the type of the move and the difference of the the utilities
         if moves[0]:
             move_num = move_mapping[moves[0]]
             temp1.append(move_num)
@@ -136,9 +155,10 @@ def update_logs(filename):
                 temp_util2.append("{0:.3f}".format(diff_prev[1]))
             temp_util2.append("{0:.3f}".format(diff[1]))
 
+        # update the list with the type of the pair of moves
         if moves[0] and moves[1]:
             move_pair_num = move_pair_mapping[(moves[0], moves[1])]
-            temp_agent1.append(move_pair_num)  # should be imported in the dataset
+            temp_agent1.append(move_pair_num)
             temp_agent2.append(move_pair_mapping[(moves[1], moves[0])])
 
     test_combined.append(temp_agent1)
@@ -150,8 +170,12 @@ def update_logs(filename):
     return test_combined, test_naive, test_naive_utils
 
 
-# another function to change the formatting
 def transform(lst):
+    """
+    Takes as input a lists of lists and returns a list of lists where the inner lists j=ha sonly one element.
+    :param lst: A list of lists.
+    :return: A list of lists in which the inner lists contain only one element.
+    """
     out = []
     for l in lst:
         for ll in l:
@@ -161,6 +185,12 @@ def transform(lst):
 
 # function used just for changing the format of the data
 def flatten(lst):
+    """
+    Takes an input a list with multiple nested lists and returns a list
+    of lists with the first element of each nested list.
+    :param lst: A list with multiple nested lists.
+    :return: A list of lists with the first element of each nested list
+    """
     out = []
     for l in lst:
         tmp = []
@@ -170,8 +200,12 @@ def flatten(lst):
     return out
 
 
-# function used to normalize the data so that they fit the gaussian standards (-1,1)
 def normalize(data):
+    """
+    Normalize the data so that they fit the gaussian standards (-1,1)
+    :param data: A list of lists
+    :return: Normalized data
+    """
     normalized = []
     for d in data:
         temp = []
@@ -181,20 +215,26 @@ def normalize(data):
     return normalized
 
 
-# the main function that produces the observations in the needed format accompanied by the length of each sequence
 def create_obs(data):
+    """
+    Produces the observations in the needed format accompanied by the length of each sequence
+    :param data: A list of lists
+    :return: Data normalized in the needed format and the length of each sequence.
+    """
     normalized = normalize(data)
     lengths = list(map(lambda x: len(x), data))
     obs = np.array(transform(normalized))
     return obs, lengths
 
 
-input_file = sys.argv[1]
+input_file = sys.argv[1]  # read the file given as input
+
+# process the logs of the negotiation and get some outcomes
 test_combined, test_naive, test_naive_utils = update_logs(input_file)
 
-# agent1, agent2 = testing_data(input_file)
-obs, lengths = create_obs(test_combined)
+obs, lengths = create_obs(test_combined)  # compute the observations and their lengths
 
+# load the trained model and its evaluation metrics
 with open('models_metrics.txt', 'rb') as f:
     model_metrics = pickle.load(f)
 
@@ -207,20 +247,26 @@ hardheaded_diff_val = model_metrics[9]
 random_diff_val = model_metrics[10]
 tft_diff_val = model_metrics[11]
 
-# testing
+# testing phase
 start = 0
 agents = ['Agent1', 'Agent2']
 
+# print the outcome for each agent and each negotiation strategy
 for ind, ln in enumerate(lengths):
     print("\n--------------  " + agents[ind] + "  --------------")
+
     pred = list(conceder_model.predict(obs[start:start + ln]))
     diff1_val = abs((pred.count(0) - pred.count(1)) / (pred.count(0) + pred.count(1)) - conceder_diff_val)
+
     pred = list(hardheaded_model.predict(obs[start:start + ln]))
     diff2_val = abs((pred.count(0) - pred.count(1)) / (pred.count(0) + pred.count(1)) - hardheaded_diff_val)
+
     pred = list(random_model.predict(obs[start:start + ln]))
     diff3_val = abs((pred.count(0) - pred.count(1)) / (pred.count(0) + pred.count(1)) - random_diff_val)
+
     pred = list(tft_model.predict(obs[start:start + ln]))
     diff4_val = abs((pred.count(0) - pred.count(1)) / (pred.count(0) + pred.count(1)) - tft_diff_val)
+
     diff = diff1_val + diff2_val + diff3_val + diff4_val
     print("Conceder: ", 1 - diff1_val / diff)
     print("Hardheaded: ", 1 - diff2_val / diff)
